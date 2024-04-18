ef98c500391d6b5c1dc5e2f6d1513f1dc98bf89f
ysubbagh <ydns10@gmail.com>
2024-04-18 15:22:58 -0700

    get basics up an running on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      // Specify the allowed domains for loading images
+      domains: ['zappysys.com', 'upload.wikimedia.org', 'cdn.jsdelivr.net', 'as1.ftcdn.net', 'cdn.pixabay.com', 'github.githubassets.com', 'ftcdn.net'],
+    },
+};
 
 export default nextConfig;
+
+  
